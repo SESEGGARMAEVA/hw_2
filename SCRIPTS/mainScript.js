8409f9ac2dcc0349aa388ac24b55ec08278cf40b
SESEG <seseggarmaeva2018@gmail.com>
2024-05-04 20:17:27 +0900

    added MainScript

---
+++ SCRIPTS/mainScript.js
@@ -0,0 +1,10 @@
+// main_script.js
+
+// Импортируем функцию min и calculateProfit из модуля userModule.js
+import { min, calculateProfit } from "./userModule.js";
+ 
+let result = min(7, 4);
+console.log(result);
+
+calculateProfit(dishs, ingredientsPrice);
+console.log(dishs);
